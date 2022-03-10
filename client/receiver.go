723b09e8750301bf9521742d2d743d654d011f91
Ad3bay0c <aclintonadebayo@gmail.com>
2022-03-10 16:43:42 +0100

    implemented consumer rabbitmq

---
+++ client/receiver.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"github.com/streadway/amqp"
+	"log"
+)
+
+func main(){
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer func() {
+		if err := conn.Close(); err != nil {
+			log.Println(err)
+		}
+	}()
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer func() {
+		if err := ch.Close(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msg, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msg {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
