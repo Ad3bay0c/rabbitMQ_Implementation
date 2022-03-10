385354221662461ef6e4528890e5aab8f9deb6e7
Ad3bay0c <aclintonadebayo@gmail.com>
2022-03-10 16:46:41 +0100

    implemented publishing with args from stdin

--- server/newTask.go
+++ server/newTask.go
@@ -1,1 +1,74 @@
 package main
+
+import (
+	"github.com/streadway/amqp"
+	"log"
+	"os"
+	"strings"
+)
+
+func failOnError1(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError1(err, "Failed to connect to RabbitMQ")
+	defer func() {
+		if err := conn.Close(); err != nil {
+			log.Fatalf("Failed to close connection: %s", err)
+		}
+	}()
+	ch, err := conn.Channel()
+	failOnError1(err, "Failed to open a channel")
+	defer func() {
+		if err := ch.Close(); err != nil {
+			log.Fatalf("Failed to close channel: %s", err)
+		}
+	}()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError1(err, "Failed to publish a message")
+
+	body := bodyFrom(os.Args)
+
+	err = ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError1(err, "Failed to publish a message")
+
+	//err = ch.Publish(
+	//	"",
+	//	q.Name,
+	//	false,
+	//	false,
+	//	amqp.Publishing{
+	//		ContentType: "text/plain",
+	//		Body:        []byte("Hello World!"),
+	//	})
+}
+
+func bodyFrom(args []string) string {
+	var s string
+	if len(args) < 2 || os.Args[1] == "" {
+		s = "hello"
+	} else {
+		s = strings.Join(args[1:], " ")
+	}
+	return s
+}
