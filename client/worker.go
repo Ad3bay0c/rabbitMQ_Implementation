96d064b84b0202c490e646ba636381f967e10c98
Ad3bay0c <aclintonadebayo@gmail.com>
2022-03-10 16:44:13 +0100

    implemented rabbitmq with time.sleep '

--- client/worker.go
+++ client/worker.go
@@ -1,1 +1,66 @@
 package main
+
+import (
+	"bytes"
+	"github.com/streadway/amqp"
+	"log"
+	"time"
+)
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError1(err, "Failed to connect to RabbitMQ")
+	defer func() {
+		if err := conn.Close(); err != nil {
+			log.Println(err)
+		}
+	}()
+	ch, err := conn.Channel()
+	failOnError1(err, "Failed to open a channel")
+	defer func() {
+		if err := ch.Close(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError1(err, "Failed to declare a queue")
+
+	msg, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msg {
+			log.Printf("Received a message: %s", d.Body)
+			dotCount := bytes.Count(d.Body, []byte("."))
+			t := time.Duration(dotCount)
+			time.Sleep(t * time.Second)
+			log.Printf("Done")
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+}
+
+func failOnError1(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
