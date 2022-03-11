00e182d0874df0294a0c8479fba8cec1912aceff
Ad3bay0c <aclintonadebayo@gmail.com>
2022-03-11 14:54:48 +0100

    implemented exchange with pub and sub

--- server/emitLog.go
+++ server/emitLog.go
@@ -1,1 +1,58 @@
 package main
+
+import (
+	"github.com/streadway/amqp"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError2(err, "Failed to connect to RabbitMQ")
+	defer func() {
+		if err := conn.Close(); err != nil {
+			log.Println(err)
+		}
+	}()
+	ch, err := conn.Channel()
+	failOnError2(err, "Failed to open a channel")
+
+	err = ch.ExchangeDeclare(
+		"logs",   // name
+		"fanout", // type
+		true,     // durable
+		false,    // auto-deleted
+		false,    // internal
+		false,    // no-wait
+		nil,      // arguments
+	)
+	failOnError2(err, "Failed to declare an exchange")
+
+	body := bodyFrom2(os.Args)
+
+	err = ch.Publish("logs", "", false, false, amqp.Publishing{
+		ContentType: "text/plain",
+		Body:        []byte(body),
+	})
+	failOnError2(err, "Failed to publish a message")
+
+	log.Printf(" [x] Sent %s", body)
+
+}
+
+func failOnError2(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func bodyFrom2(args []string) string {
+	var s string
+	if len(args) < 2 || os.Args[1] == "" {
+		s = "hello"
+	} else {
+		s = strings.Join(args[1:], " ")
+	}
+	return s
+}
